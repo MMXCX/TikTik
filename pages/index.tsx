118b63bc26f7676c722dda45b5132bc855396d1b
MMXCX <mmxcx@gmail.com>
2022-09-19 00:38:04 +0300

    01:29:45

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,33 @@
 import type { NextPage } from 'next'
+import axios from 'axios'
+import { Video } from '../types'
 
-const Home: NextPage = () => {
+interface IProps {
+  videos: Video[]
+}
+
+const Home = ({ videos }: IProps) => {
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      TikTik
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video:Video) => (
+          <VideoCard post={video} key={video._id}/>
+        ))
+      ) : (
+        <NoResults text="No Videos"/>
+      )}
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`)
+  return {
+    props: {
+      videos: data
+    }
+  }
 }
 
 export default Home
